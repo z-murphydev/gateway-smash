d6ad736560ea939c9e8ae8f623fafae5b14d9161
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-11-23 10:47:39 -0800

    Pushing for Nich

---
+++ client/src/pages/Rules/Rules.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Rules = () => (
+  <>
+    <h1>
+      <strong>Rules</strong>
+    </h1>
+    <ul>
+      <li>
+        Be respectful~ (Shit talk is allowed and encouraged, but keep it reasonable. If the person clearly does not
+        enjoy/reciprocate. Stop.)
+      </li>
+      <li>All Johns (excuses) lead to negative points. Points deducted depends on the level of the John.</li>
+      <li>
+        Let's keep it clean. There's a lot of people and it can/will get hot. Please shower before you come, bring deodorant
+        and use it liberally.
+      </li>
+    </ul>
+  </>
+);
+
+export default Rules;
