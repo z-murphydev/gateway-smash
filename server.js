0c2e6d1b95ac2386401e64178f303e01fc611576
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-11-22 19:16:53 -0800

    Server up.

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 require('dotenv').config();
+const dbConn = require('./db');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const PORT = process.env.PORT || 6969;
+
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+dbConn()
+	.then(() => {
+		console.log("Connected to DB.");
+		app.listen(PORT, () =>
+			console.log(`APP LISTENING FOR CONNECTIONS ON PORT: ${PORT}`)
+		);
+	})
+	.catch(error => {
+		console.log("Could not start APP error:", error);
+	});
