1df0503b9ab1c07301b26a566b867eecb7cf77a4
Munch-Z <46868472+Munch-Z@users.noreply.github.com>
2019-11-23 16:40:24 -0800

    Overview done. Just needs styles.

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,26 +1,31 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import { Container } from "layout";
-import Rules from "./Rules";
-import Leaderboard from "./Leaderboard";
-import Missions from "./Missions";
+import { Container } from 'layout';
+import Rules from './Rules';
+import Leaderboard from './Leaderboard';
+import Missions from './Missions';
+import Overview from './Overview';
 
 interface Props {}
 
 const index: React.FC<Props> = () => (
-  <Container as="main">
+  <Container as='main'>
     <Switch>
-      <Route exact path="/rules">
+      <Route exact path='/rules'>
         <Rules />
       </Route>
 
-      <Route exact path="/leaderboard">
+      <Route exact path='/leaderboard'>
         <Leaderboard />
       </Route>
 
-      <Route path="/challenges">
+      <Route exact path='/missions'>
         <Missions />
+      </Route>
+
+      <Route exact path='/'>
+        <Overview />
       </Route>
     </Switch>
   </Container>
